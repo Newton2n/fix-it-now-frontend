7849877e15b1f893f47e08fce534607b80f9a949
Newton <156936866+Newton2n@users.noreply.github.com>
2026-08-09 18:38:45 +0600

    refactor: update metadata description and enhance user profile handling in navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "FixItNow",
-  description: "Your Trusted Home Service Platform",
+  description: "Your Trusted Service Platform",
 };
 
 export default async function RootLayout({
--- src/components/shared/auth-navbar.tsx
+++ src/components/shared/auth-navbar.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from "react";
+
 import { getMe } from "@/actions/auth.action";
+
 import Navbar from "./navbar";
 
 export default function AuthNavbar() {
@@ -12,20 +14,29 @@ export default function AuthNavbar() {
 
 async function AuthNavbarContent() {
   const user = await getMe();
+  console.log("user details", user);
 
   return (
     <Navbar
       role={user?.data?.role ?? null}
-      userName={user?.data?.name}
+      userName={user?.data?.name ?? null}
+      profilePicture={
+        user?.data?.profilePicture ??
+        "https://images.unsplash.com/vector-1745610393569-9373c9c64117?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww"
+      }
     />
   );
 }
 
 function NavbarSkeleton() {
   return (
-    <div className="flex h-16 w-full items-center justify-between px-4 border-b bg-background">
+    <div className="flex h-16 w-full items-center justify-between border-b bg-background px-4">
       <div className="h-6 w-24 animate-pulse rounded bg-muted" />
-      <div className="h-8 w-8 animate-pulse rounded-full bg-muted" />
+
+      <div className="flex items-center gap-2">
+        <div className="h-8 w-8 animate-pulse rounded-full bg-muted" />
+        <div className="hidden h-8 w-16 animate-pulse rounded bg-muted sm:block" />
+      </div>
     </div>
   );
 }
